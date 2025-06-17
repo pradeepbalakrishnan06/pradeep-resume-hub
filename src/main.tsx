9b8e4f261803d7ecca4a2c5ad55bc3d0d597795a
Pradeep <67412106+pradeepbalakrishnan06@users.noreply.github.com>
2025-06-17 18:55:21 +0530

    Update main.tsx
    Add google analytics measurement id / code

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,37 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+// Google Analytics Setup
+declare global {
+  interface Window {
+    dataLayer: any[];
+    gtag: (...args: any[]) => void;
+  }
+}
+
+// Initialize Google Analytics
+function initializeGA() {
+  
+  const GA_MEASUREMENT_ID = 'G-4B5J7LRS5K';
+  
+  // Load Google Analytics script
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
+  document.head.appendChild(script);
+  
+  // Initialize dataLayer and gtag function
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = function() {
+    window.dataLayer.push(arguments);
+  };
+  
+  // Configure Google Analytics
+  window.gtag('js', new Date());
+  window.gtag('config', GA_MEASUREMENT_ID);
+}
+
+// Initialize GA before rendering the app
+initializeGA();
 
 createRoot(document.getElementById("root")!).render(<App />);
